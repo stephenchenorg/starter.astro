b05d9bc13ac5b4f714dce1ce98bf938397e44942
Lucas Yang <yangchenshin77@gmail.com>
2024-12-06 13:53:14 +0800

    Add middleware to response 404 status

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { defineMiddleware } from 'astro/middleware'
+import { GraphQLRequestError } from 'awesome-graphql-client'
+
+export const onRequest = defineMiddleware(async (context, next) => {
+  try {
+    return await next()
+  } catch (e) {
+    // Handle 404 errors
+    if (e instanceof GraphQLRequestError && e.message.includes('Http Status 404')) {
+      return new Response(null, {
+        status: 404,
+        statusText: 'Not found',
+      })
+    }
+
+    throw e
+  }
+})
