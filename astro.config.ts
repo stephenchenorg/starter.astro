d40d4d4a40c7b322c949b06a521f29b2c2899249
Lucas Yang <yangchenshin77@gmail.com>
2025-03-04 00:26:31 +0800

    Add site for config

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import netlify from '@astrojs/netlify'
 
 export default defineConfig({
+  site: 'http://localhost:4321',
   output: 'server',
   adapter: netlify(),
 })
