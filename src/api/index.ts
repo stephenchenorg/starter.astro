d2648f561071d04f42ec8fa9356eb8fc81507e0b
Lucas Yang <yangchenshin77@gmail.com>
2025-03-03 15:23:11 +0800

    Using `createGraphQLAPI()` to create `graphQLAPI()` function

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,7 @@
-export * from './fetch'
+import { createGraphQLAPI } from '@stephenchenorg/astro/api'
+
+export const graphQLAPI = createGraphQLAPI({
+  endpoint: `${import.meta.env.API_BASE_URL.replace(/\/$/, '')}/graphql`,
+})
+
+export { gql } from '@stephenchenorg/astro/api'
