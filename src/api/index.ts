89e9d0ba8e73f3b9b4b1e5436e921ae7f97db207
Lucas Yang <yangchenshin77@gmail.com>
2025-03-27 12:42:58 +0800

    Update to new api error handler

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,4 +4,4 @@ export const graphQLAPI = createGraphQLAPI({
   endpoint: `${import.meta.env.API_BASE_URL.replace(/\/$/, '')}/graphql`,
 })
 
-export { gql } from '@stephenchenorg/astro/api'
+export { gql, GraphQLValidationError } from '@stephenchenorg/astro/api'
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,13 @@
 import { defineMiddleware } from 'astro/middleware'
-import { GraphQLRequestError } from '@stephenchenorg/astro/api'
+import { handleErrorResponse } from '@stephenchenorg/astro/api'
 
 export const onRequest = defineMiddleware(async (_context, next) => {
   try {
     return await next()
   } catch (e) {
-    // Handle 404 errors
-    if (e instanceof GraphQLRequestError && e.isNotFound()) {
-      return new Response(null, {
-        status: 404,
-        statusText: 'Not found',
-      })
+    const response = handleErrorResponse(e)
+    if (response) {
+      return response
     }
 
     throw e
