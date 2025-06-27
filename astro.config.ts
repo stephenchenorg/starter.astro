fcbc9fa27718f0cb8e2eec7bb212da63130be168
tasb00429 <tasb00429@gmail.com>
2025-06-27 14:14:22 +0800

    feat:  add tailwindcss

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,13 @@
 import netlify from '@astrojs/netlify'
 import { defineConfig } from 'astro/config'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: 'http://localhost:4321',
   output: 'server',
+  vite: {
+    plugins: [tailwindcss()],
+  },
   trailingSlash: 'never',
   build: {
     format: 'file',
