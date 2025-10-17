403dbb745ed7253d04fca73ea21499f068067c0b
tasb00429 <tasb00429@gmail.com>
2025-10-18 01:31:34 +0800

    Fast update using script

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,4 +4,8 @@ export default {
 
   apiBaseUrl: import.meta.env.API_BASE_URL.replace(/\/$/, ''),
   ga4TrackingId: import.meta.env.GA4_TRACKING_ID,
+  gtmId: import.meta.env.GTM_ID,
+  lineTagId: import.meta.env.LINE_TAG_ID,
+  clarityId: import.meta.env.CLARITY_ID,
+  fbPixelId: import.meta.env.FB_PIXEL_ID,
 }
