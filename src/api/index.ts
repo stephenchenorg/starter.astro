385c997d89c81d6da813af7f4fa39a62d47226b0
Lucas Yang <yangchenshin77@gmail.com>
2025-12-19 03:01:51 +0800

    Bump @stephenchenorg/astro to v9.0

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,27 @@
-import { createGraphQLAPI } from '@stephenchenorg/astro/api'
+import type { APIContext } from 'astro'
+import type { FetchOptions, MappedResponseType, ResponseType } from 'ofetch'
+import { ofetch } from 'ofetch'
+import siteConfig from '@/site.config'
 
-export const graphQLAPI = createGraphQLAPI({
-  endpoint: `${import.meta.env.API_BASE_URL.replace(/\/$/, '')}/graphql`,
-})
+export async function apiFetch<T = any, R extends ResponseType = 'json'>(
+  url: string,
+  userOptions: FetchOptions<R> & { Astro?: APIContext } = {}
+): Promise<MappedResponseType<R, T>> {
+  const { Astro: astroContext, ...options } = userOptions
 
-export { gql, GraphQLValidationError } from '@stephenchenorg/astro/api'
+  const response = await ofetch(url, {
+    baseURL: siteConfig.apiBaseUrl,
+    ...options,
+    async onRequest({ request, options }) {
+      options.headers.set('Content-Language', 'zh-TW')
+      options.headers.set('Time-Zone', 'Asia/Taipei')
+      options.headers.set('Referer', 'https://dentalk.com.tw/')
+
+      // if (userToken?.token) {
+      //   options.headers.set('Authorization', `Bearer ${userToken.token}`)
+      // }
+    },
+  })
+
+  return response
+}
